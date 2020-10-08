46f6ba1de5b8db8772dfd50db2a1f5c4a0a90ac0
Brian Vega <dentvega6@gmail.com>
2020-10-07 20:21:06 -0400

    Configuracion de redux

---
+++ src/redux/reducers/config.reducer.js
@@ -0,0 +1,23 @@
+import { CHANGE_TAB_INDEX, CLEAN_ALL_REDUCERS } from '../actions/actions';
+
+const initState = {
+    tab: 0,
+};
+
+function configReducer(state = initState, action) {
+    switch (action.type) {
+        case CHANGE_TAB_INDEX: {
+            return {
+                ...state,
+                tab: action.index
+            };
+        }
+        case CLEAN_ALL_REDUCERS: {
+            return initState;
+        }
+        default:
+            return state;
+    }
+}
+
+export default configReducer;
