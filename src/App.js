ed97d2e5f21b8089ad9eafc9cdcea70f1b8f68a0
Brian Vega <dentvega6@gmail.com>
2020-10-11 11:08:15 -0400

    Creacion del AppBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
 import RouterMain from './router/RouterMain';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import red from '@material-ui/core/colors/red';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2D3540',
+    },
+    secondary: {
+      main: red[500],
+    },
+  },
+});
 
 function App() {
   return (
-    <RouterMain/>
+    <ThemeProvider theme={theme}>
+      <RouterMain/>
+    </ThemeProvider>
   );
 }
 
---
+++ src/constants/strings.js
@@ -0,0 +1,5 @@
+export const sTittleApp = 'App';
+export const sNameUmss = 'Universidad de San Simon';
+export const sLogin = 'Iniciar Sesion';
+export const sProfile = 'Perfil';
+export const sNotifications = 'Notificaciones';
