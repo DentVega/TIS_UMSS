54d25ee4a2011eb05171f9e36fb5dd36f63ba4c5
Brian Dennis Vega Hidalgo <dentvega6@gmail.com>
2020-10-24 08:21:51 -0400

    - VALIDACIONES

--- src/constants/formCustomHook/useForm.js
+++ src/constants/formCustomHook/useForm.js
@@ -1,11 +1,15 @@
 import { useState } from 'react';
+import { emailRegex, nameRegex, lastNameRegex, phoneRegex, ciRegex } from '../regexs';
 
 export const useEmail = () => {
   const [values, setValues] = useState('');
   const [emailError, setEmailError] = useState(false);
   const [emailMessage, setEmailMessage] = useState('');
   const handleEmailChange = (value) => {
-    if (values.length > 29) {
+    if (!emailRegex.test(value)) {
+      setEmailError(true);
+      setEmailMessage('Debe seguir el siguiente formato test@example.com');
+    } else if (value.length > 29) {
       setEmailError(true);
       setEmailMessage('Muchos Caracteres');
     } else {
@@ -20,7 +24,7 @@ export const useEmail = () => {
 export const usePassword = () => {
   const [values, setValues] = useState('');
   const [passwordError, setPasswordError] = useState(false);
-  const [passMessage,setPassMessage] = useState('');
+  const [passMessage, setPassMessage] = useState('');
 
   const validPass = () => {
     return values.length > 20 || values.length === 0;
@@ -36,55 +40,102 @@ export const usePassword = () => {
     }
     setValues(target.value);
   };
-  return [values, handlePassChange, passwordError,setPasswordError,passMessage,setPassMessage ];
+  return [values, handlePassChange, passwordError, setPasswordError, passMessage, setPassMessage];
 };
 
-export const useFullName=()=>{
+export const useFullName = () => {
   const [values, setValues] = useState('');
-  const [fullNameError,setFullNameError] = useState(false);
-  const [fullNameMesasge,setFullNameErrorMessage] = useState('');
-  const handleFullNameChange =(value)=>{
-    if(values.length > 24){
-      setFullNameErrorMessage("limite de caracteres 25");
+  const [fullNameError, setFullNameError] = useState(false);
+  const [fullNameMesasge, setFullNameErrorMessage] = useState(null);
+  const handleFullNameChange = (value) => {
+    if (value.length === 0) {
+      setFullNameErrorMessage('El nombre no puede estar vacio');
       setFullNameError(true);
-    }else{
-      setFullNameErrorMessage("");
+    } else if (!nameRegex.test(value)) {
+      setFullNameErrorMessage('Solo se aceptan letras');
+      setFullNameError(true);
+    } else if (value.length > 0 && value.length <= 2) {
+      setFullNameErrorMessage('El nombre debe tener mas de 2 caracteres');
+      setFullNameError(true);
+    } else if (value.length > 24) {
+      setFullNameErrorMessage('limite de caracteres 25');
+      setFullNameError(true);
+    } else {
+      setFullNameErrorMessage(null);
       setFullNameError(false);
     }
     setValues(value);
-  }
-  return [values,handleFullNameChange,fullNameError,setFullNameError,fullNameMesasge,setFullNameErrorMessage];
-}
+  };
+  return [values, handleFullNameChange, fullNameError, setFullNameError, fullNameMesasge, setFullNameErrorMessage];
+};
 
-export const useCi=()=>{
+export const useLastName = () => {
+  const [values, setValues] = useState('');
+  const [fullNameError, setFullNameError] = useState(false);
+  const [fullNameMesasge, setFullNameErrorMessage] = useState(null);
+  const handleFullNameChange = (value) => {
+    if (value.length === 0) {
+      setFullNameErrorMessage('El Apellido no puede estar vacio');
+      setFullNameError(true);
+    } else if (!lastNameRegex.test(value)) {
+      setFullNameErrorMessage('Solo se aceptan letras');
+      setFullNameError(true);
+    } else if (value.length > 0 && value.length <= 2) {
+      setFullNameErrorMessage('El Apellido debe tener mas de 2 caracteres');
+      setFullNameError(true);
+    } else if (value.length > 24) {
+      setFullNameErrorMessage('limite de caracteres 25');
+      setFullNameError(true);
+    } else {
+      setFullNameErrorMessage(null);
+      setFullNameError(false);
+    }
+    setValues(value);
+  };
+  return [values, handleFullNameChange, fullNameError, setFullNameError, fullNameMesasge, setFullNameErrorMessage];
+};
+
+export const useCi = () => {
   const [values, setValues] = useState('');
   const [ciError, setCiError] = useState(false);
   const [ciErrorMessage, setCiMessageError] = useState('');
-  const handleCiChange =(value)=>{
-    if(values.length > 7){
-      setCiMessageError("8 numeros como maximo");
+  const handleCiChange = (value) => {
+    if (!ciRegex.test(value)) {
+      setCiMessageError('Solo se permiten numeros');
       setCiError(true);
-    }else{
-      setCiMessageError("");
+    } else if (value.length < 5) {
+      setCiMessageError('Debe ser mayor a 5 numeros');
+      setCiError(true);
+    } else if (value.length > 7) {
+      setCiMessageError('8 numeros como maximo');
+      setCiError(true);
+    } else {
+      setCiMessageError('');
       setCiError(false);
     }
     setValues(value);
-  }
-  return [values,handleCiChange,ciError, setCiError,ciErrorMessage, setCiMessageError];
-}
-export const usePhone=()=>{
+  };
+  return [values, handleCiChange, ciError, setCiError, ciErrorMessage, setCiMessageError];
+};
+export const usePhone = () => {
   const [values, setValues] = useState('');
-  const [phoneError,setPhoneError] = useState(false);
-  const [phoneErrorMessage,setPhoneErrorMessage] =useState("");
-  const handlePhoneChange =(value)=>{
-    if(values.length > 7){
-      setPhoneErrorMessage("8 numeros como maximo");
+  const [phoneError, setPhoneError] = useState(false);
+  const [phoneErrorMessage, setPhoneErrorMessage] = useState('');
+  const handlePhoneChange = (value) => {
+    if (!phoneRegex.test(value)) {
+      setPhoneErrorMessage('Solo se aceptan numeros');
       setPhoneError(true);
-    }else{
-      setPhoneErrorMessage("");
+    } else if (value.length < 7) {
+      setPhoneErrorMessage('7 numeros como minimo');
+      setPhoneError(true);
+    } else if (value.length >= 8) {
+      setPhoneErrorMessage('8 numeros como maximo');
+      setPhoneError(true);
+    } else {
+      setPhoneErrorMessage('');
       setPhoneError(false);
     }
     setValues(value);
-  }
-  return [values,handlePhoneChange,phoneError,setPhoneError,phoneErrorMessage,setPhoneErrorMessage]
-}
+  };
+  return [values, handlePhoneChange, phoneError, setPhoneError, phoneErrorMessage, setPhoneErrorMessage];
+};
--- src/constants/regexs.js
+++ src/constants/regexs.js
@@ -1,1 +1,6 @@
-export const emailregex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+// export const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i;
+export const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i;
+export const nameRegex = /^[a-zA-Z ]+$/;
+export const lastNameRegex = /^[a-zA-Z ]+$/;
+export const phoneRegex = /^[0-9]+$/;
+export const ciRegex = /^[0-9]+$/;
